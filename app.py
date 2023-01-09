66ccb0b14ff6090f7456e722c81b41122c9c4ce6
gitsugatensho <zkhan@zaizi.com>
2023-01-09 17:13:33 +0000

    final changes

--- app.py
+++ app.py
@@ -8,22 +8,25 @@ app = Flask(__name__)
 #static values
 words = ['the', 'of', 'to', 'and', 'a', 'in', 'is', 'it', 'you', 'that', 'he', 'was', 'for', 'on', 'are', 'with', 'as', 'I', 'his', 'they', 'be', 'at', 'one', 'have', 'this', 'from', 'or', 'had', 'by', 'not', 'word', 'but', 'what', 'some', 'we', 'can', 'out', 'other', 'were', 'all', 'market', 'degree', 'populate', 'chick', 'dear', 'enemy', 'reply', 'drink', 'occur', 'support', 'speech', 'nature', 'range', 'steam', 'motion', 'path', 'liquid', 'log', 'meant', 'teeth', 'shell', 'neck']
 
-@app.route('/')
-def index():
+def randomise():
     # choosing a random gif
-    # numm = str(random.randint(1,16))
-    imageurl = "https://www.gasta.org/wordpress/wp-content/uploads/2021/11/Anim0"+numm+"_dribbble.gif"
+    numm = str("%02d" % (random.randint(1,13),))
+    imageurl = "https://www.gasta.org/wordpress/wp-content/uploads/2021/11/Anim"+numm+"_dribbble.gif"
     # making user + comments                                                             
     comments = []
     for x in range(random.randint(2,6)):
         user = "User" + str(random.randint(0,9999))
         comment = ''
-        comment = ' '.join([random.choice(words) for y in range(random.randint(2,9))])#+(' \U0001F602'*random.randint(0,4))
-        #em = ' &#128514; ' * random.randint(0,4)
-        #em = emoji.emojize(':joy:') * random.randint(0,4)
-        amount = random.randint(0,4)
+        comment = ' '.join([random.choice(words) for y in range(random.randint(2,16))])
+        amount = random.choice([0,1,2,3,4,0,1,2,3,1,2,7])
         comments.append([user, comment, amount])
-    return render_template('index.html', url=imageurl, comments=comments)
+    return [imageurl, comments]
+randomise()
+
+@app.route('/')
+def index():
+    values = randomise()
+    return render_template('index.html', url=values[0], comments=values[1])
 
 if __name__ == "__main__": 
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=False)
